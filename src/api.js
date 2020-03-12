56e0be4b3f89a1b4cfa4c44a5363a937ff3c621c
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-03-12 13:53:37 -0400

    refactor: Cleans up api.

--- src/api.js
+++ src/api.js
@@ -1,12 +1,13 @@
 import { CLIEngine as ESLintCLIEngine } from "eslint"
 
-import { getOptionsSync } from "./utils/config"
-
-export { Linter, RuleTester, SourceCode } from "eslint"
+import { getCLIOptionsSync } from "./utils/cli"
 
 export class CLIEngine extends ESLintCLIEngine {
   constructor(providedOptions) {
-    const finalOptions = getOptionsSync([], providedOptions)
-    super(finalOptions)
+    const cliOptions = getCLIOptionsSync([], providedOptions)
+    super(cliOptions)
   }
 }
+export { getPrettierConfig } from "./utils/prettier"
+export { getESLintConfig, getESLintConfigSync } from "./utils/eslint"
+export { Linter, RuleTester, SourceCode } from "eslint"
